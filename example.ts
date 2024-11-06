dd4c17858f4370b008f7f1dfef840570e72cb4a9
robin.boeckmann <robin.boeckmann@flixbus.com>
2024-11-06 23:59:37 +0100

    Add code for destructuring

--- example.ts
+++ example.ts
@@ -2,19 +2,27 @@
 interface GreetingProps {
     name: string;
     age?: number; // optionales Feld
-  }
-  
-  // Funktion, die einen Begrüßungstext zurückgibt
-  function greet(props: GreetingProps): string {
+}
+
+// Funktion, die einen Begrüßungstext zurückgibt
+function greet(props: GreetingProps): string {
+    // Destruktierung von Objekten
     const { name, age } = props;
+
+    // Destrukturierung von Arrays
+    const cities = ["Berlin", "Hamburg", "München", "Köln", "Frankfurt"]
+    const [first, second] = cities
+    // const first = list[0]
+    // const second = list[1]
+
     if (age) {
-      return `Hello, ${name}! You are ${age} years old.`;
+        return `Hello, ${name}! You are ${age} years old and live in ${first}.`;
     } else {
-      return `Hello, ${name}!`;
+        return `Hello, ${name}! You live in ${second}.`;
     }
-  }
-  
-  // Aufruf der Funktion
-  const message = greet({ name: "Robin", age: 30 });
-  console.log(message);
-  
+}
+
+// Aufruf der Funktion
+const message = greet({ name: "Robin", age: 30 });
+console.log(message);
+
